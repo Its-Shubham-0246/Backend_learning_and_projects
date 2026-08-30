453666cc1af1d70df90f55c7a169d80851b00b3e
Its-Shubham_0246 <shubhammaurya2406@gmail.com>
2026-08-30 23:32:00 +0530

    learning about routing requests, taking user input and redirecting requests

---
+++ RequestAndResponse/practice.js
@@ -0,0 +1,36 @@
+http = require('http');
+
+const server = http.createServer((req,res) => {
+  console.log(req.url, req.method);
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Myntra</title></head>');
+  res.write('<body>');
+  res.write('<header>');
+  res.write('<nav>');
+  res.write('<a href="/">home</a>');
+  res.write('<a href="/men">men</a>');
+  res.write('<a href="/women">women</a>');
+  res.write('<a href="/kids">kids</a>');
+  res.write('<a href="/cart">cart</a>');
+  res.write('</nav>');
+  res.write('</header>');
+  if(req.url === '/'){
+    res.write('<h1>Welcome to Home Page</h1>')
+  }else if(req.url === '/men'){
+    res.write('<h1>Welcome to the men page</h1>');
+  }else if(req.url === '/women'){
+    res.write('<h1>Welcome to the Women page</h1>');
+  }else if(req.url === '/kids'){
+    res.write('<h1>Welcome to the Kids page</h1>');
+  }else if(req.url === '/cart'){
+    res.write('<h1>Welcome to the cart page</h1>');
+  }
+  res.write('</body>');
+  res.write('</html>');
+  res.end();
+});
+const PORT = 3001;
+server.listen(PORT,()=>{
+  console.log(`Server is Running at address http://localhost:${PORT}`);
+})
